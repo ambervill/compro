6e414f497872163ef791c261ccff16bb2f361c0e
Andrew <alekseychuk@oviont.ru>
2014-07-04 23:30:47 +0400

    added competence.rb, lesson.rb

--- app/models/competence.rb
+++ app/models/competence.rb
@@ -1,7 +1,7 @@
 class Competence < ActiveRecord::Base
-  
+
   has_many :discipline_competences
-  has_many :disciplines, through: :discipline_competences 
+  has_many :disciplines, through: :discipline_competences
   has_many :speciality_competences
-  has_many :specialities, through: :speciality_competences 
+  has_many :specialities, through: :speciality_competences
 end
--- db/migrate/20140704181857_create_join_table_competence_lesson.rb
+++ db/migrate/20140704181857_create_join_table_competence_lesson.rb
@@ -1,8 +1,8 @@
 class CreateJoinTableCompetenceLesson < ActiveRecord::Migration
   def change
     create_join_table :competences, :lessons do |t|
-      # t.index [:competence_id, :lesson_id]
-      # t.index [:lesson_id, :competence_id]
+       t.index [:competence_id, :lesson_id]
+       t.index [:lesson_id, :competence_id]
     end
   end
 end
